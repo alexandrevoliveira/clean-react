367cc2131185ce86d0084f079773c63e209c3a8c
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2022-09-21 11:55:10 -0300

    test: ensure RemoteAuthentication returns an Account on success

--- src/domain/test/mock-account.ts
+++ src/domain/test/mock-account.ts
@@ -1,7 +1,12 @@
+import { AccountModel } from '@/domain/models/account-model'
 import { AuthenticationParams } from '@/domain/usecases/authentication'
 import faker from 'faker'
 
 export const mockAuthentication = (): AuthenticationParams => ({
   email: faker.internet.email(),
   password: faker.internet.password()
+})
+
+export const mockAccountModel = (): AccountModel => ({
+  accessToken: faker.datatype.uuid()
 })
