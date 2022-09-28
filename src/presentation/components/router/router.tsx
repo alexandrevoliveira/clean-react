813578d21a42037618ac565caf118dc16fb5c399
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2022-09-28 19:13:06 -0300

    feat: add app router

---
+++ src/presentation/components/router/router.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Login } from '@/presentation/pages'
+import '../../styles/global.scss'
+
+const Router: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default Router
