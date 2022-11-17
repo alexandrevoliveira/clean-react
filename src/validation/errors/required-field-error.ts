56f1433c2300eb43717694dd35e1d43f75ecfc7a
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2022-11-16 22:36:30 -0300

    feat: ensure EmailValidation returns error if email is invalid

--- src/validation/errors/required-field-error.ts
+++ src/validation/errors/required-field-error.ts
@@ -1,6 +1,6 @@
 export class RequiredFieldError extends Error {
   constructor () {
     super('Campo obrigatório')
-    this.message = 'RequiredFieldError'
+    this.name = 'RequiredFieldError'
   }
 }
