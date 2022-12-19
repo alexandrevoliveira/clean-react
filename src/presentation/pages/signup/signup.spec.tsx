881e487ec6608ac70b3517d95f80118ee59945b9
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2022-12-19 20:10:44 -0300

    test: ensure SignUp show valid passwordConfirmation state if validation succeeds

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -84,4 +84,10 @@ describe('SignUp Component', () => {
     Helper.populateField(sut, 'password')
     Helper.testStatusForField(sut, 'password')
   })
+
+  it('should show valid passwordConfirmation state if Validation succeeds', () => {
+    const { sut } = makeSut()
+    Helper.populateField(sut, 'passwordConfirmation')
+    Helper.testStatusForField(sut, 'passwordConfirmation')
+  })
 })
