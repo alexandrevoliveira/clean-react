fa760031a152b9818dfb6084bd8a1b136f5c4634
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2022-12-22 19:18:26 -0300

    test: add SignUpValidationFactory

---
+++ src/main/factories/pages/signup/signup-validation-factory.spec.ts
@@ -0,0 +1,14 @@
+import { ValidationBuilder as Builder, ValidationComposite } from '@/validation/validators'
+import { makeSignUpValidation } from './signup-validation-factory'
+
+describe('SignUpValidationFactory', () => {
+  it('should make ValidationComposite with correct validations', () => {
+    const composite = makeSignUpValidation()
+    expect(composite).toEqual(ValidationComposite.build([
+      ...Builder.field('name').required().min(5).build(),
+      ...Builder.field('email').required().email().build(),
+      ...Builder.field('password').required().min(5).build(),
+      ...Builder.field('passwordConfirmation').required().min(5).build()
+    ]))
+  })
+})
