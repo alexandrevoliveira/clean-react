6286ba8880330540a63d7d8eb6eb50b6a02f391e
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2022-12-27 14:52:39 -0300

    chore: add cypress command

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   fixturesFolder: false,
   e2e: {
     baseUrl: 'http://localhost:8080',
-    supportFile: false,
+    supportFile: 'src/main/test/cypress/support/index.js',
     specPattern: 'src/main/test/cypress/e2e/**/*.{js,jsx,ts,tsx}'
   }
 })
