beeedd8861e0902f84961fd511be11435e2a336a
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2023-01-02 16:32:14 -0300

    test: ensure Login will not submit form if its invalid

--- src/main/test/cypress/e2e/login.spec.ts
+++ src/main/test/cypress/e2e/login.spec.ts
@@ -118,4 +118,15 @@ describe('Login', () => {
     cy.getByTestId('submit').dblclick()
     cy.get('@request.all').should('have.length', 1)
   })
+
+  it('should not call submit if form is invalid', () => {
+    cy.intercept('POST', /login/, {
+      statusCode: 200,
+      body: {
+        accessToken: faker.datatype.uuid()
+      }
+    }).as('request')
+    cy.getByTestId('email').focus().type(faker.internet.email()).type('{enter}')
+    cy.get('@request.all').should('have.length', 0)
+  })
 })
