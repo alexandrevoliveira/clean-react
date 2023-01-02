81f6fa327f7ffd5ec6f667524d8ea5c8621bf106
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2023-01-02 16:16:24 -0300

    test: prevent Login to call submit multiple times

--- src/main/test/cypress/e2e/login.spec.ts
+++ src/main/test/cypress/e2e/login.spec.ts
@@ -106,4 +106,17 @@ describe('Login', () => {
     cy.url().should('eq', `${baseUrl}/`)
     cy.window().then(window => assert.isOk(window.localStorage.getItem('accessToken')))
   })
+
+  it('should prevent multiple submits', () => {
+    cy.intercept('POST', /login/, {
+      statusCode: 200,
+      body: {
+        accessToken: faker.datatype.uuid()
+      }
+    }).as('request')
+    cy.getByTestId('email').focus().type(faker.internet.email())
+    cy.getByTestId('password').focus().type(faker.random.alphaNumeric(5))
+    cy.getByTestId('submit').dblclick()
+    cy.get('@request.all').should('have.length', 1)
+  })
 })
