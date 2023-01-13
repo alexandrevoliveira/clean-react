0cc61b46af77e442a552ad0336831ee629fd3243
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2023-01-13 17:45:25 -0300

    refactor: change cache storage to sync

--- src/infra/cache/local-storage-adapter.spec.ts
+++ src/infra/cache/local-storage-adapter.spec.ts
@@ -9,11 +9,11 @@ describe('LocalStorageAdapter', () => {
     localStorage.clear()
   })
 
-  it('should call localStorage with correct values', async () => {
+  it('should call localStorage with correct values', () => {
     const sut = makeSut()
     const key = faker.database.column()
     const value = faker.random.word()
-    await sut.set(key, value)
+    sut.set(key, value)
     expect(localStorage.setItem).toHaveBeenCalledWith(key, value)
   })
 })
