73826117d02507ec1e8e53f7ac260a1e485f4560
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2023-02-26 21:07:50 -0300

    test: ensure PrivateRoute renders component if token is not empty

--- src/presentation/components/private-route/private-route.spec.tsx
+++ src/presentation/components/private-route/private-route.spec.tsx
@@ -1,21 +1,31 @@
-import React from 'react'
-import { render } from '@testing-library/react'
+import { mockAccountModel } from '@/domain/test'
+import { ApiContext } from '@/presentation/contexts'
+import { SurveyList } from '@/presentation/pages'
 import PrivateRoute from './private-route'
+import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { render } from '@testing-library/react'
 
-const makeSut = (): void => {
+const makeSut = (account = mockAccountModel()): void => {
   render(
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<PrivateRoute />} />
-      </Routes>
-    </BrowserRouter>
+    <ApiContext.Provider value={{ getCurrentAccount: () => account }}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<PrivateRoute><SurveyList /></PrivateRoute>} />
+        </Routes>
+      </BrowserRouter>
+    </ApiContext.Provider>
   )
 }
 
 describe('PrivateRoute', () => {
-  it('should redirect to /login if token is empty', () => {
+  it('should render current component if token is not empty', () => {
     makeSut()
+    expect(window.location.pathname).toBe('/')
+  })
+
+  it('should redirect to /login if token is empty', () => {
+    makeSut(null)
     expect(window.location.pathname).toBe('/login')
   })
 })
