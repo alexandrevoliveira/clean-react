839bf3ba7d8a720f36ff015f8165ee945073610e
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2023-07-12 23:17:55 -0300

    test: ensure SurveyList render SurveyItems on success

--- src/presentation/pages/survey-list/survey-list.spec.tsx
+++ src/presentation/pages/survey-list/survey-list.spec.tsx
@@ -1,20 +1,22 @@
 import { SurveyModel } from '@/domain/models'
+import { mockSurveyListModel } from '@/domain/test'
 import { LoadSurveyList } from '@/domain/usecases'
 import { SurveyList } from '@/presentation/pages'
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 import React from 'react'
-
-type SutTypes = {
-  loadSurveyListSpy: LoadSurveyListSpy
-}
 
 class LoadSurveyListSpy implements LoadSurveyList {
   callsCount = 0
+  surveys = mockSurveyListModel()
 
   async loadAll (): Promise<SurveyModel[]> {
     this.callsCount++
-    return []
+    return this.surveys
   }
+}
+
+type SutTypes = {
+  loadSurveyListSpy: LoadSurveyListSpy
 }
 
 const makeSut = (): SutTypes => {
@@ -26,14 +28,23 @@ const makeSut = (): SutTypes => {
 }
 
 describe('SurveyList Component', () => {
-  it('should present 4 empty items on start', () => {
+  it('should present 4 empty items on start', async () => {
     makeSut()
     const surveyList = screen.getByTestId('survey-list')
-    expect(surveyList.querySelectorAll('li:empty').length).toBe(4)
+    expect(surveyList.querySelectorAll('li:empty')).toHaveLength(4)
+    await waitFor(() => surveyList)
   })
 
-  it('should call LoadSurveyList', () => {
+  it('should call LoadSurveyList', async () => {
     const { loadSurveyListSpy } = makeSut()
     expect(loadSurveyListSpy.callsCount).toBe(1)
+    await waitFor(() => screen.getByRole('heading'))
+  })
+
+  it('should render SurveyItems on success', async () => {
+    makeSut()
+    const surveyList = screen.getByTestId('survey-list')
+    await waitFor(() => surveyList)
+    await waitFor(() => expect(surveyList.querySelectorAll('li.surveyItemWrap')).toHaveLength(3))
   })
 })
