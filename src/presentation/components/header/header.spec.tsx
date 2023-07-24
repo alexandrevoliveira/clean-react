cd2a53edc7765e38987fc47db7cbefa871a31e68
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2023-07-24 20:36:01 -0300

    refactor: move SUT creation to a factory method

--- src/presentation/components/header/header.spec.tsx
+++ src/presentation/components/header/header.spec.tsx
@@ -1,19 +1,31 @@
+import { AccountModel } from '@/domain/models'
 import { Header } from '@/presentation/components'
 import { ApiContext } from '@/presentation/contexts'
 import { fireEvent, render, screen } from '@testing-library/react'
 import { BrowserRouter } from 'react-router-dom'
 import React from 'react'
 
+type SutTypes = {
+  setCurrentAccountMock: (account: AccountModel) => void
+}
+
+const makeSut = (): SutTypes => {
+  const setCurrentAccountMock = jest.fn()
+  render(
+    <ApiContext.Provider value={{ setCurrentAccount: setCurrentAccountMock }}>
+      <BrowserRouter window={window} >
+        <Header />
+      </BrowserRouter>
+    </ApiContext.Provider>
+  )
+  return {
+    setCurrentAccountMock
+  }
+}
+
 describe('Header Component', () => {
   it('should call setCurrentAccount with null', async () => {
-    const setCurrentAccountMock = jest.fn()
-    render(
-      <ApiContext.Provider value={{ setCurrentAccount: setCurrentAccountMock }}>
-        <BrowserRouter window={window} >
-          <Header />
-        </BrowserRouter>
-      </ApiContext.Provider>
-    )
+    const { setCurrentAccountMock } = makeSut()
     fireEvent.click(screen.getByTestId('logout'))
     expect(setCurrentAccountMock).toHaveBeenCalledWith(undefined)
     expect(window.location.pathname).toBe('/login')
