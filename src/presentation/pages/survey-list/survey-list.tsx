300473a42472a88e604ef1486d68020e61eba862
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2023-07-24 21:44:17 -0300

    feat: ensure SurveyList logout on AccessDeniedError

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -1,14 +1,19 @@
 import Styles from './survey-list-styles.scss'
+import { AccessDeniedError } from '@/domain/errors'
 import { LoadSurveyList } from '@/domain/usecases'
 import { Footer, Header } from '@/presentation/components'
+import { ApiContext } from '@/presentation/contexts'
 import { Error, SurveyContext, SurveyListItem } from '@/presentation/pages/survey-list/components'
-import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import React, { useContext, useEffect, useState } from 'react'
 
 type Props = {
   loadSurveyList: LoadSurveyList
 }
 
 const SurveyList: React.FC<Props> = ({ loadSurveyList }: Props) => {
+  const { setCurrentAccount } = useContext(ApiContext)
+  const navigate = useNavigate()
   const [state, setState] = useState({
     surveys: [] as LoadSurveyList.Model[],
     error: '',
@@ -18,7 +23,14 @@ const SurveyList: React.FC<Props> = ({ loadSurveyList }: Props) => {
   useEffect(() => {
     loadSurveyList.loadAll()
       .then(surveys => setState({ ...state, surveys }))
-      .catch(error => setState({ ...state, error: error.message }))
+      .catch(error => {
+        if (error instanceof AccessDeniedError) {
+          setCurrentAccount(undefined)
+          navigate('/login', { replace: true })
+        } else {
+          setState({ ...state, error: error.message })
+        }
+      })
   }, [state.reload])
 
   return (
