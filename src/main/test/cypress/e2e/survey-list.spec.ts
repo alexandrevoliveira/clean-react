c7bf84f15a0856626cd8e374ee24c65f14070eaf
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2023-07-30 22:43:21 -0300

    test: ensure SurveyList reload on button click

--- src/main/test/cypress/e2e/survey-list.spec.ts
+++ src/main/test/cypress/e2e/survey-list.spec.ts
@@ -23,6 +23,15 @@ describe('Login', () => {
     cy.getByTestId('error').should('contain.text', 'Algo de errado aconteceu. Tente novamente em breve')
   })
 
+  it('should reload on button click', () => {
+    mockUnexpectedError()
+    cy.visit('')
+    cy.getByTestId('error').should('contain.text', 'Algo de errado aconteceu. Tente novamente em breve')
+    mockSuccess()
+    cy.getByTestId('reload').click()
+    cy.get('li:not(:empty)').should('have.length', 2)
+  })
+
   it('should logout on AccessDeniedError', () => {
     mockAccessDeniedError()
     cy.visit('')
