8252525789743a094ee83aa36dda6c30d7d780ed
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2023-08-13 23:06:28 -0300

    refactor: move Calendar to its own Component

---
+++ src/presentation/components/calendar/calendar.spec.tsx
@@ -0,0 +1,23 @@
+import { Calendar } from '@/presentation/components'
+import { render, screen } from '@testing-library/react'
+import React from 'react'
+
+const makeSut = (date: Date): void => {
+  render(<Calendar date={date}/>)
+}
+
+describe('Calendar Component', () => {
+  it('should render with correct values', () => {
+    makeSut(new Date('2023-01-10T00:00:00'))
+    expect(screen.getByTestId('day')).toHaveTextContent('10')
+    expect(screen.getByTestId('month').textContent).toBe('jan')
+    expect(screen.getByTestId('year')).toHaveTextContent('2023')
+  })
+
+  it('should render icon and day with correct values', () => {
+    makeSut(new Date('2019-05-03T00:00:00'))
+    expect(screen.getByTestId('day')).toHaveTextContent('03')
+    expect(screen.getByTestId('month').textContent).toBe('mai')
+    expect(screen.getByTestId('year')).toHaveTextContent('2019')
+  })
+})
--- src/presentation/components/index.ts
+++ src/presentation/components/index.ts
@@ -1,3 +1,4 @@
+export { default as Calendar } from './calendar/calendar'
 export { default as Footer } from './footer/footer'
 export { default as FormStatus } from './form-status/form-status'
 export { default as Header } from './header/header'
