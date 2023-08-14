0f65d0f663e96d0682421cb0e5eb4854e50df1cc
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2023-08-13 22:40:51 -0300

    refactor: change setState to use callback

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -40,7 +40,7 @@ const Login: React.FC<Props> = ({ validation, authentication }) => {
       if (state.isLoading || state.isFormInvalid) {
         return
       }
-      setState({ ...state, isLoading: true })
+      setState(old => ({ ...old, isLoading: true }))
       const account = await authentication.auth({
         email: state.email,
         password: state.password
@@ -48,11 +48,11 @@ const Login: React.FC<Props> = ({ validation, authentication }) => {
       setCurrentAccount(account)
       navigate('/', { replace: true })
     } catch (error) {
-      setState({
-        ...state,
+      setState(old => ({
+        ...old,
         isLoading: false,
         mainError: error.message
-      })
+      }))
     }
   }
 
