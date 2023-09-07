4eda5ca53fa1a86587ba06d2d42631216888d508
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2023-09-07 00:39:17 -0300

    feat: ensure SurveyResult calls SaveSurveyResult on non active answer click

---
+++ src/main/factories/usecases/save-survey-result/remote-save-survey-result-factory.ts
@@ -0,0 +1,8 @@
+import { RemoteSaveSurveyResult } from '@/data/usecases'
+import { SaveSurveyResult } from '@/domain/usecases'
+import { makeAuthorizeHttpClientDecorator } from '@/main/factories/decorators'
+import { makeApiUrl } from '@/main/factories/http'
+
+export const makeRemoteSaveSurveyResult = (id: string): SaveSurveyResult => {
+  return new RemoteSaveSurveyResult(makeApiUrl(`/surveys/${id}/results`), makeAuthorizeHttpClientDecorator())
+}
