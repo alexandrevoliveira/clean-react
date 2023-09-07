c2e1107d28f92c9bbb4cbd34484554cdb582526b
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2023-09-07 16:06:39 -0300

    refactor: remove flip move

--- src/presentation/pages/survey-result/components/result/result.tsx
+++ src/presentation/pages/survey-result/components/result/result.tsx
@@ -2,7 +2,6 @@ import Styles from './result-styles.scss'
 import { LoadSurveyResult } from '@/domain/usecases'
 import { Calendar } from '@/presentation/components'
 import { SurveyResultAnswer } from '@/presentation/pages/survey-result/components'
-import FlipMove from 'react-flip-move'
 import { useNavigate } from 'react-router-dom'
 import React from 'react'
 
@@ -18,11 +17,9 @@ const Result: React.FC<Props> = ({ surveyResult }) => {
         <Calendar date={surveyResult.date} className={Styles.calendarWrap} />
         <h2 data-testid="question">{surveyResult.question}</h2>
       </hgroup>
-      <FlipMove data-testid="answers" className={Styles.answersList}>
-        <>
-          {surveyResult.answers.map(answer => <SurveyResultAnswer key={answer.answer} answer={answer}/>)}
-        </>
-      </FlipMove>
+      <ul data-testid="answers" className={Styles.answersList}>
+        {surveyResult.answers.map(answer => <SurveyResultAnswer key={answer.answer} answer={answer}/>)}
+      </ul>
       <button className={Styles.button} data-testid="back-button" onClick={() => navigate(-1)}>Voltar</button>
     </>
   )
