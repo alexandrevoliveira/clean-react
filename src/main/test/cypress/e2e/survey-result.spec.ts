546809668b036dbc543d33239a803cea92389f0e
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2023-09-07 15:39:59 -0300

    test: ensure SurveyResult logout on AccessDeniedError

--- src/main/test/cypress/e2e/survey-result.spec.ts
+++ src/main/test/cypress/e2e/survey-result.spec.ts
@@ -68,6 +68,7 @@ describe('SurveyResult', () => {
 
   describe('save', () => {
     const mockUnexpectedError = (): void => Http.mockServerError(path, 'PUT')
+    const mockAccessDeniedError = (): void => Http.mockForbiddenError(path, 'PUT')
 
     beforeEach(() => {
       cy.fixture('account').then(account => {
@@ -81,6 +82,12 @@ describe('SurveyResult', () => {
       mockUnexpectedError()
       cy.get('li:nth-child(2)').click()
       cy.getByTestId('error').should('contain.text', 'Algo de errado aconteceu. Tente novamente em breve')
+    })
+
+    it('should logout on AccessDeniedError', () => {
+      mockAccessDeniedError()
+      cy.get('li:nth-child(2)').click()
+      Helper.testUrl('/login')
     })
   })
 })
