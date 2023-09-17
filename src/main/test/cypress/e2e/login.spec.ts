06b2b2c99f1016fef8fa3f9b016c8cf25b5d81ff
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2023-09-17 20:22:56 -0300

    test: reset loginState on page load

--- src/main/test/cypress/e2e/login.spec.ts
+++ src/main/test/cypress/e2e/login.spec.ts
@@ -35,6 +35,14 @@ describe('Login', () => {
     cy.getByTestId('error-wrap').should('not.have.descendants')
   })
 
+  it('should reset state on page load', () => {
+    cy.getByTestId('email').focus().type(faker.internet.email())
+    FormHelper.testInputStatus('email')
+    cy.getByTestId('signup-link').click()
+    cy.getByTestId('login-link').click()
+    FormHelper.testInputStatus('email', 'Campo obrigatório')
+  })
+
   it('should present error state if form is invalid', () => {
     cy.getByTestId('email').focus().type(faker.random.word())
     FormHelper.testInputStatus('email', 'Valor inválido')
